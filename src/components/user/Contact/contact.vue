776471f5daee11fcef57c6ac1f2b3aebaff13f65
abdelrahmanMagdy <abdelrahman.magdy307@gmail.com>
2023-06-16 21:47:25 +0300

    update in contact component

--- src/components/user/Contact/contact.vue
+++ src/components/user/Contact/contact.vue
@@ -1,32 +1,19 @@
 <template>
-   <!DOCTYPE html>
-  <html lang="">
-  
-  <head>
-      <meta charset="UTF-8">
-      <meta name="description" content="Ogani Template">
-      <meta name="keywords" content="Ogani, unica, creative, html">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <meta http-equiv="X-UA-Compatible" content="ie=edge">
-      <title>Ogani | Template</title>
-  
-      <!-- Google Font -->
-      <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@200;300;400;600;900&display=swap" rel="stylesheet">
-  
-      <!-- Css Styles -->
-      <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
-      <link rel="stylesheet" href="css/font-awesome.min.css" type="text/css">
-      <link rel="stylesheet" href="css/elegant-icons.css" type="text/css">
-      <link rel="stylesheet" href="css/nice-select.css" type="text/css">
-      <link rel="stylesheet" href="css/jquery-ui.min.css" type="text/css">
-      <link rel="stylesheet" href="css/owl.carousel.min.css" type="text/css">
-      <link rel="stylesheet" href="css/slicknav.min.css" type="text/css">
-      <link rel="stylesheet" href="/css/style.css" type="text/css">
-  </head>
-  
-  <body>
-  
+    <html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Complete Responsive Grocery Store Website</title>
 
+    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+    <link rel="stylesheet" href="/css/style.css">
+
+</head>
+<body>
    <section class="contact " id="contact" >
 
       <h1 class="heading"> contact <span> now </span> </h1>
